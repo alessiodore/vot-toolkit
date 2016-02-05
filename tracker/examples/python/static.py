6bddd8a4bff29e8791b23b711197691c1ec97b86
alessiodore <alessio@wirewax.com>
2016-02-05 12:07:13 +0000

    added comments

--- tracker/examples/python/static.py
+++ tracker/examples/python/static.py
@@ -1,6 +1,12 @@
 """
 \file static.py
 
+@brief Example of tracker integration. Static tracker 
+
+@author Alessio Dore
+
+@date 2016
+
 """
 
 import vot
@@ -9,15 +15,13 @@ import time
 def static():  
     options = vot.trax.TraxServerOptions('static', 'v1', vot.trax.TRAX_REGION_RECTANGLE, vot.trax.TRAX_IMAGE_PATH)   
     with vot.VOT(options, True) as pyvot: 
-        #pyvot.trax_server_setup()           
-        #msgType, msgArgs = pyvot.trax_server_wait( )          
-        
         initRegion, initImgPath = pyvot.vot_initialize()            
         while True:                
             imgPath = pyvot.frame()
             if not imgPath:
                 break
             pyvot.report(initRegion)
+            # @fixme: the evaluation breaks if it runs too fast
             time.sleep(0.01)
     return 0       
 
--- tracker/examples/python/vot.py
+++ tracker/examples/python/vot.py
@@ -1,6 +1,11 @@
 """
 \file vot.py
 
+@brief Wrap TraX functions for tracker integration
+
+@author Alessio Dore
+
+@date 2016
 
 """
 
@@ -8,7 +13,7 @@ import trax
 
 
 class VOT(trax.TraxServer):
-    """  """
+    """ Extend TraX server wrapping some server functions for tracker integration """
     def __init__(self, options, verbose = False):
         """ Constructor
         
@@ -21,9 +26,12 @@ class VOT(trax.TraxServer):
         
     def vot_initialize(self):
         """
+        Send configuration message to the client and receive the initialization 
+        region and the path of the first image 
         
         Returns:
-            region:
+            region: region as vot_region_rect or vot_region_poly object
+            imgPath: path of the first image
         """        
         self.trax_server_setup()           
         msgType, msgArgs = self.trax_server_wait( )  
@@ -44,15 +52,17 @@ class VOT(trax.TraxServer):
 
     def report(self, region):
         """
+        Report the tracking results to the client
         
         Args:
-            region:   
+            region: pass region as vot_region_rect or vot_region_poly     
         """
-        assert(isinstance(region, vot_region_rect))
+        assert(isinstance(region, vot_region))
         self.trax_server_reply(str(region))
         
     def frame(self):
         """
+        Get a frame (image path) from client 
         
         Returns:
             imgPath: absolute path of the image
@@ -66,26 +76,27 @@ class VOT(trax.TraxServer):
         
     
 class vot_region(object):
-    """ """
+    """ Base class for vot region """
     def __init__(self):
         pass
         
     def parseRegionStr(self, regionStr):
-        """ """
+        """ In derived classes implement method to parse region string """
         return
             
 class vot_region_rect(vot_region):
-    """ """
+    """ Rectangle region """
     def __init__(self):
         super(vot_region_rect)
         self.regionType = trax.TRAX_REGION_RECTANGLE
         self.x, self.y, self.w, self.h = 0, 0, 0, 0
 
     def __str__(self):
+        """ Create string from class to send to client """
         return '{},{},{},{}'.format(self.x, self.y, self.w, self.h)
         
     def parseRegionStr(self, regionStr):
-        """ """
+        """ Parse region string to get x, y, w, h """
         self.x, self.y, self.w, self.h = map(float, regionStr.strip('"').split(','))   
             
 class vot_region_poly(vot_region):
@@ -97,4 +108,4 @@ class vot_region_poly(vot_region):
         
     def parseRegionStr(self, regionStr):
         """ """
-        
+        pass
